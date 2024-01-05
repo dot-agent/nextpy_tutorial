bd1d0fe1af1ffd95b3716c4d6d3498ba04d8445f
shravya-34 <shettyshravya34@gmail.com>
2024-01-06 00:21:44 +0530

    component

---
+++ frontend/component.py
@@ -0,0 +1,42 @@
+# Nextpy Frontend 
+## Components in Nextpy
+### Put this code to your blank template main page.
+
+import nextpy as xt
+
+def another_component() -> xt.Component:
+    return xt.hstack(
+        xt.box(
+            bg = 'green',
+            height = '30vh',
+            width = '30vh'
+        ),
+        xt.box(
+            bg = 'red',
+            height = '30vh',
+            width = '30vh'
+        )
+    )
+
+def index() -> xt.Component:
+    return xt.fragment(
+        xt.box(
+            xt.text(
+                'Text in box',
+                color = 'white'
+            ),
+            bg = 'black',
+            height = '40vh',
+            width = '40vh'
+        ),
+        another_component(),
+        xt.center(
+            xt.text('Center'),
+            bg = 'yellow',
+            height = '50vh',
+            width = '50vh'
+        )
+    )
+
+app = xt.App()
+app.add_page(index)
